fdbcd2fbbb43774ef929c68b35244ce4d1fd8caa
Jim Mullen <jim@idimensionz.com>
2015-09-01 23:31:35 -0400

    Initial attempt at authentication implementation

---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+// Needed for isolated tests
+/** @var $loader \Composer\Autoload\ClassLoader */
+$loader = require dirname(__DIR__) . '/vendor/autoload.php';
+$loader->add('Tests\\iDimensionz\\SendGridWebApiV3\\', __DIR__.'/tests');
+
+
+
